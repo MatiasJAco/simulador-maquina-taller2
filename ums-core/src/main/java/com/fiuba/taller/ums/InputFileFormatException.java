72c74d73b22859174dc363ec1a1ccd2c0a2a8b5c
MatiasJA@gmail.com <MatiasJA@gmail.com@c530f557-eb08-89a8-6938-bd18804ee936>
2014-04-19 18:29:11 +0000

    Clases que faltaban.

---
+++ ums-core/src/main/java/com/fiuba/taller/ums/InputFileFormatException.java
@@ -0,0 +1,10 @@
+package com.fiuba.taller.ums;
+
+public class InputFileFormatException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ ums-core/src/main/java/com/fiuba/taller/ums/InstructionParametersException.java
@@ -0,0 +1,8 @@
+package com.fiuba.taller.ums;
+
+public class InstructionParametersException extends RuntimeException {
+
+	public InstructionParametersException(String message) {  
+	      super(message);  
+	   }  
+}
