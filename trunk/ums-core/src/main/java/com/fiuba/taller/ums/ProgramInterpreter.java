a01086af5312f8e420a2a72344b4fdc442d3479a
MatiasJA@gmail.com <MatiasJA@gmail.com@c530f557-eb08-89a8-6938-bd18804ee936>
2014-05-02 01:02:40 +0000

    -Se separa el interprete en 2 clases. -La clase SyntaxChecker se ocupa del chequeo de sintaxis y devuelve una instruccion maq si se le da una asm. -Interpreter solo maneja administracion de archivos y es intermediario entre la gui y el Checker. -Se agregan casos de prueba. -Se agregan logs detallados de errores e informativos.

--- trunk/ums-core/src/main/java/com/fiuba/taller/ums/ProgramInterpreter.java
+++ trunk/ums-core/src/main/java/com/fiuba/taller/ums/ProgramInterpreter.java
@@ -8,183 +8,35 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class ProgramInterpreter {
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
+public class ProgramInterpreter {
+	private Logger log;
 	private static final String FILESYSTEM_SEPARATOR = "\\\\";
 	private static final String ASM_EXT = ".asm";
 	private static final String CODABS_EXT = ".mq";
 
-	private enum MustUseSwitch{
-		LDM,
-		LDI, 
-		STM, 
-		COP, 
-		ADD, 
-		ADDF,
-		OR,
-		AND,
-		XOR, 
-		ROTD,
-		JPZ, 
-		RET;	    
-	}
-
-	String censura(String texto, String tabu, int tipo){ 
-
-		while(texto.contains(tabu))
-		{
-			int inicio = texto.indexOf(tabu);
-			int fin = inicio + tabu.length();
-			StringBuffer aux = new StringBuffer();
-			switch(tipo){
-			case 0 : { 
-				texto=texto.substring(0,inicio).concat(texto.substring(fin));
-				texto=texto.trim();
-				break;
-			}
-			case 1 : {
-				for (int i = 0; i < tabu.length(); i++) aux.append('*');				
-				texto.replace(tabu, aux);
-				break;
-			}
-			default :break; 
-			}
-		}
-
-		return texto;
+	public ProgramInterpreter() {
+		BasicConfigurator.configure();
+		log = Logger.getLogger("UMS Log");
 	}
 
 	public String interpret (String inst) throws InputFileFormatException{
 		//ldm R,XY
-		String[] instructionArray = inst.split(" ");
-		String instName = instructionArray[0];
-		String instParam="";
-		if (instructionArray.length > 1)
-			instParam = instructionArray[1];
-		String opCode = this.getOpCode(instName);
-		String param = this.getParameters(instParam, opCode.charAt(0));
-		String result = opCode + param;
-
-		return result;
-	}
-
-	private String getParameters(String instParam, char opCode) {
-		String[] paramArray = instParam.split(",");
-		String result = "";
-		try{
-			validarCantidadParametros(paramArray, opCode);
-		}
-		catch (InstructionParametersException ex){
-			System.out.println(ex.getMessage());
-			return ex.getMessage();
-		}
-		for (int i = 0; i < paramArray.length ; i++){
-			try {
-				validarParameterSize(paramArray[i]);
-				result = result + paramArray[i];
-			}catch (InstructionParametersException ex){
-				System.out.println(ex.getMessage());
-				return ex.getMessage();
-			}
-
-		};		
-		result=parseSpecialInstructions(result,opCode);
-		return result;
-	}
-
-	private void validarParameterSize(String string) throws InstructionParametersException {
-		if (string.length() > 2){
-			String msg = "Tamanio de parametro excedido. Supera los 2 bytes. Tamanio: " + string.length() + ".";
-			throw new InstructionParametersException(msg);
-		}
-
-	}
-
-	private void validarCantidadParametros(String[] paramArray, char opCode) throws InstructionParametersException {
-		if(paramArray.length >3){
-			String msg = "Cantidad de parametros excedida. Hay "+ paramArray.length + " parametros.";
-			throw new InstructionParametersException(msg);
-		}
-		if(paramArray.length <2 && opCode != 'C'){
-			String msg = "Cantidad de parametros insuficiente. Hay "+ paramArray.length + " parametros.";
-			throw new InstructionParametersException(msg);
-		}		
-	}
-
-	private String parseSpecialInstructions(String result, char opCode) {
-		switch (opCode){
-		case '4':
-			result = '0' + result;
-			break;
-		case 'A':
-			result = result.substring(0, 1)+ '0' + result.substring(1, result.length());
-			break;
-		case 'C':
-			result = result + "000";
-			break;
-		case 'X':
+		String result="";
+		
+		if(SyntaxChecker.checkAssembly(inst)){
+			result = SyntaxChecker.getMaqInstruction(inst);
+		}else{
 			result = "ERROR";
-		default: ;		
 		}
-
+		
+		
 
 		return result;
 	}
-
-	private String getOpCode (String instName) throws InputFileFormatException {
-		String result = null;
-		//switch (instName){
-		try{
-			switch(MustUseSwitch.valueOf(instName.toUpperCase())){
-			case LDM:
-				result="1";
-				break;
-			case LDI: 
-				result="2";
-				break;
-			case STM:
-				result="3";
-				break;
-			case COP:
-				result="4";
-				break;
-			case ADD:
-				result="5";
-				break;
-			case ADDF:
-				result="6";
-				break;
-			case OR:
-				result="7";
-				break;
-			case AND:
-				result="8";
-				break;
-			case XOR:
-				result="9";
-				break;
-			case ROTD:
-				result="A";
-				break;
-			case JPZ:
-				result="B";
-				break;
-			case RET:
-				result="C";
-				break;		
-			default:break;		
-
-			}
-		}catch(InputFileFormatException exc){
-			System.out.println("El archivo input no tiene el formato aceptado.");
-
-		}
-		catch(IllegalArgumentException exc){
-			System.out.println("El archivo input no tiene el formato aceptado.");
-			return "X";
-		}
-		return result;
-	}
+	
 
 	public void generateAbsoluteCodeFile(String inputFile) {
 		// The name of the file to open.
@@ -236,7 +88,11 @@ public class ProgramInterpreter {
 	}
 
 	private String generateOutputFilename(String inputFile) {
-		String result = "";
+		String result = "";		
+//		BasicConfigurator.configure();
+//        log = Logger.getLogger("UMS Log");
+//		log.warn("un warning");
+//        log.error("un error");
 		result = inputFile.replace(ASM_EXT,CODABS_EXT);
 		return result;
 	}
--- trunk/ums-core/src/test/java/com/fiuba/taller/ums/TestProgramInterpreter.java
+++ trunk/ums-core/src/test/java/com/fiuba/taller/ums/TestProgramInterpreter.java
@@ -12,79 +12,87 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestProgramInterpreter {
-
-	@Test
-	public void testCensura() {
-		ProgramInterpreter pi = new ProgramInterpreter();
-		String result= pi.censura("Esto es un test esto es.", "test", 0);
-		assertEquals("Esto es un  esto es.",result);
-	}
-
-
+	
+	private ProgramInterpreter pi;
+	
+	@Before
+    public void init() {
+		pi = new ProgramInterpreter();
+    }
+	
 	@Test
 	public void testInterpretarCargaM() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("ldm R,XY");
 		assertEquals("1RXY",result);
 	}
 
 	@Test
 	public void testInterpretarCargaI() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("ldi R,XY");
 		assertEquals("2RXY",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarAlmacen() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("stm R,XY");
 		assertEquals("3RXY",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarCopia() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("cop R,S");
 		assertEquals("40RS",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarSuma() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("add R,S,T");
 		assertEquals("5RST",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarSumaF() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("addf R,S,T");
 		assertEquals("6RST",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarOr() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("or R,S,T");
 		assertEquals("7RST",result);
-	}@Test
+	}
+	
+	
+	@Test
 	public void testInterpretarAnd() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("and R,S,T");
 		assertEquals("8RST",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarXor() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("xor R,S,T");
 		assertEquals("9RST",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarRotd() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("rotd R,X");
 		assertEquals("AR0X",result);
-	}@Test
+	}
+	
+	@Test
 	public void testInterpretarSalta() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("jpz R,XY");
 		assertEquals("BRXY",result);
 	}
+	
 	@Test
 	public void testInterpretarParar() throws InputFileFormatException {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		String result= pi.interpret("ret");
 		assertEquals("C000",result);
 	}
@@ -93,7 +101,6 @@ public class TestProgramInterpreter {
 
 	@Test
 	public void testGenerarCodigoMaquina() {
-		ProgramInterpreter pi = new ProgramInterpreter();
 		pi.generateAbsoluteCodeFile("C:\\temp\\input.asm");
 	}
 
