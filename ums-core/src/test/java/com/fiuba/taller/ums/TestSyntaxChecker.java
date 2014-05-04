17afea10ce69764cb3555cd952893f4281dc4f54
MatiasJA@gmail.com <MatiasJA@gmail.com@c530f557-eb08-89a8-6938-bd18804ee936>
2014-05-04 08:58:23 +0000

    -Se agregan nuevos chequeos para valores que no sean hexadecimales señalandolos como error. -Nuevo archivo de prueba que no sa error.

--- ums-core/src/test/java/com/fiuba/taller/ums/TestSyntaxChecker.java
+++ ums-core/src/test/java/com/fiuba/taller/ums/TestSyntaxChecker.java
@@ -19,58 +19,58 @@ public class TestSyntaxChecker {
 	
 	@Test
 	public void testChequearAssemblerCargaM() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("ldm R,XY");
+		boolean result= SyntaxChecker.checkAssembly("ldm 3,8E");
 		assertTrue(result);
 	}
 
 	@Test
 	public void testChequearAssemblerCargaI() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("ldi R,XY");
+		boolean result= SyntaxChecker.checkAssembly("ldi 3,8E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerAlmacen() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("stm R,XY");
+		boolean result= SyntaxChecker.checkAssembly("stm 3,8E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerCopia() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("cop R,S");
+		boolean result= SyntaxChecker.checkAssembly("cop 3,A");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerSuma() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("add R,S,T");
+		boolean result= SyntaxChecker.checkAssembly("add 3,A,2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerSumaF() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("addf R,S,T");
+		boolean result= SyntaxChecker.checkAssembly("addf 3,A,2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerOr() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("or R,S,T");
+		boolean result= SyntaxChecker.checkAssembly("or 3,A,2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerAnd() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("and R,S,T");
+		boolean result= SyntaxChecker.checkAssembly("and 3,A,2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerXor() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("xor R,S,T");
+		boolean result= SyntaxChecker.checkAssembly("xor 3,A,2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerRotd() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("rotd R,X");
+		boolean result= SyntaxChecker.checkAssembly("rotd 3,8");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearAssemblerSalta() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkAssembly("jpz R,XY");
+		boolean result= SyntaxChecker.checkAssembly("jpz 3,8E");
 		assertTrue(result);
 	}
 	@Test
@@ -81,62 +81,62 @@ public class TestSyntaxChecker {
 	
 	@Test
 	public void testChequearMaqCargaM() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("1RXY");
+		boolean result= SyntaxChecker.checkMaq("00  138E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqCargaI() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("2RXY");
+		boolean result= SyntaxChecker.checkMaq("00  238E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqAlmacen() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("3RXY");
+		boolean result= SyntaxChecker.checkMaq("00  338E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqCopia() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("40RS");
+		boolean result= SyntaxChecker.checkMaq("00  403A");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqSuma() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("5RST");
+		boolean result= SyntaxChecker.checkMaq("00  53A2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqSumaF() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("6RST");
+		boolean result= SyntaxChecker.checkMaq("00  63A2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqOr() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("7RST");
+		boolean result= SyntaxChecker.checkMaq("00  73A2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqAnd() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("8RST");
+		boolean result= SyntaxChecker.checkMaq("00  83A2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqXor() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("9RST");
+		boolean result= SyntaxChecker.checkMaq("00  93A2");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqRotd() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("AR0X");
+		boolean result= SyntaxChecker.checkMaq("00  A308");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqSalta() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("BRXY");
+		boolean result= SyntaxChecker.checkMaq("00  B38E");
 		assertTrue(result);
 	}
 	@Test
 	public void testChequearMaqParar() throws InputFileFormatException {
-		boolean result= SyntaxChecker.checkMaq("C000");
+		boolean result= SyntaxChecker.checkMaq("00  C000");
 		assertTrue(result);
 	}	
 	
