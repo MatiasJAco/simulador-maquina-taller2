b596146e0afdb350f9e0ca58ebd20c4d9b286e9a
MatiasJA@gmail.com <MatiasJA@gmail.com@c530f557-eb08-89a8-6938-bd18804ee936>
2014-05-10 22:29:53 +0000

    -Se agrega soporte para operaciones de punto flotante.

--- trunk/ums-core/src/main/java/com/fiuba/taller/ums/FPUnit.java
+++ trunk/ums-core/src/main/java/com/fiuba/taller/ums/FPUnit.java
@@ -5,7 +5,9 @@ public class FPUnit {
 	static final int BITS_MANTISA = 4;
 	static final int BITS_EXPONENTE = 3;
 	static final int BITS_SIGNO = 1;
-	
+//	static final int EXCESO = ((2^BITS_EXPONENTE)-1)/2;
+	static final int EXCESO = 3;
+
 	static float puntoFlotanteADecimal(String numeroHexa){
 		//TODO Control de errores
 		float result = 0;
@@ -17,28 +19,40 @@ public class FPUnit {
 		String mantisa = myBinary.substring(BITS_SIGNO+BITS_EXPONENTE,BITS_SIGNO+BITS_EXPONENTE+BITS_MANTISA);
 		int matSigno=1;
 		//Cambiar polaridad de matSigno si signo es 0;
-		
-		
+
 		//Calcula exponente
 		int matExpo= HexaConverter.baseToDecimal(exponente,2);
+		matExpo=matExpo-EXCESO;
 		//Calcula mantisa
-		int matMantisa = HexaConverter.baseToDecimal(mantisa,2);
-		
-		
-		
-		//Calculo del valor
-		
+//		int matMantisa = HexaConverter.baseToDecimal(mantisa,2);
+		String numBin = getOriginalBinary(mantisa, matExpo);
+
+		//Calculo del valor	
+		result = HexaConverter.baseToDecimalF(numBin, 2);
+
+
 		return result;
-		
+
 	}
 
-	private static String hexaToBinary(String numeroHexa) {
+	private static String getOriginalBinary(String mantisa, int matExpo) {
 		// TODO Auto-generated method stub
-		String result = "";
-		
+		String result = "1";
+
+		for (int i = 0; i < matExpo; i++){
+			if(mantisa.length()== 0){
+				result += "0";
+			}else{
+				result += mantisa.charAt(0);
+				mantisa = mantisa.substring(1);
+			}
+		}
+		if (mantisa.length()>0)
+			result += "." + mantisa;
 		
 		return result;
-	};
-	
-	
+	}
+
+
+
 }
