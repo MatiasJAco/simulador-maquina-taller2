3a271d7d74cca18617db1f64fa41d5d87fb6d4fc
MatiasJA@gmail.com <MatiasJA@gmail.com@c530f557-eb08-89a8-6938-bd18804ee936>
2014-05-12 03:19:14 +0000

    -Se agrega soporte para operaciones de punto flotante con signo negativo, y otras correcciones.

--- ums-core/src/main/java/com/fiuba/taller/ums/FPUnit.java
+++ ums-core/src/main/java/com/fiuba/taller/ums/FPUnit.java
@@ -5,8 +5,8 @@ public class FPUnit {
 	static final int BITS_MANTISA = 4;
 	static final int BITS_EXPONENTE = 3;
 	static final int BITS_SIGNO = 1;
-//	static final int EXCESO = ((2^BITS_EXPONENTE)-1)/2;
-	static final int EXCESO = 3;
+	//	static final int EXCESO = ((2^BITS_EXPONENTE)-1)/2;
+	static final int EXCESO = 4;
 
 	static float puntoFlotanteADecimal(String numeroHexa){
 		//TODO Control de errores
@@ -17,42 +17,88 @@ public class FPUnit {
 		String signo = myBinary.substring(0,BITS_SIGNO);
 		String exponente = myBinary.substring(BITS_SIGNO,BITS_SIGNO+BITS_EXPONENTE);
 		String mantisa = myBinary.substring(BITS_SIGNO+BITS_EXPONENTE,BITS_SIGNO+BITS_EXPONENTE+BITS_MANTISA);
-		int matSigno=1;
+		int matSigno=getSign(signo);
 		//Cambiar polaridad de matSigno si signo es 0;
 
 		//Calcula exponente
 		int matExpo= HexaConverter.baseToDecimal(exponente,2);
 		matExpo=matExpo-EXCESO;
 		//Calcula mantisa
-//		int matMantisa = HexaConverter.baseToDecimal(mantisa,2);
+		//		int matMantisa = HexaConverter.baseToDecimal(mantisa,2);
 		String numBin = getOriginalBinary(mantisa, matExpo);
 
 		//Calculo del valor	
-		result = HexaConverter.baseToDecimalF(numBin, 2);
+		result = HexaConverter.baseToDecimalF(numBin, 2) * matSigno;
 
 
 		return result;
 
+	}
+
+	private static int getSign(String signo) {
+		int result=1;
+		if(signo.equals("1"))
+			result=result*(-1);
+
+		return result;
 	}
 
 	private static String getOriginalBinary(String mantisa, int matExpo) {
-		// TODO Auto-generated method stub
 		String result = "1";
-
-		for (int i = 0; i < matExpo; i++){
-			if(mantisa.length()== 0){
-				result += "0";
-			}else{
-				result += mantisa.charAt(0);
-				mantisa = mantisa.substring(1);
+		if(matExpo >= 0){
+			for (int i = 0; i < matExpo; i++){
+				if(mantisa.length()== 0){
+					result += "0";
+				}else{
+					result += mantisa.charAt(0);
+					mantisa = mantisa.substring(1);
+				}
 			}
+			if (mantisa.length()>0)
+				result += "." + mantisa;
+
+		}else{
+
+			for (int i = 0; i < (matExpo*(-1)); i++){
+				result = "0" + result;
+			}
+			result = result + mantisa;
+			result = result.substring(0,0) + "." + result.substring(1); 
 		}
-		if (mantisa.length()>0)
-			result += "." + mantisa;
-		
+
 		return result;
 	}
 
+
+	static String decimalAPuntoFlotante(float num){
+		String result = "";
+		String sign= "0";
+		if (num < 0){
+			sign = "1";
+			num=num*-1;
+		}
+
+		String myBinNum =HexaConverter.decimalToBaseF(num, 2, 8);
+		//Normalizar
+		int difPos=myBinNum.indexOf(".")-myBinNum.indexOf("1");
+		int exp=0;
+		if(difPos >0)
+			exp=difPos-1;
+		else
+			exp=difPos;
+		
+		String expBin = HexaConverter.decimalToBase(exp + EXCESO, 2, BITS_EXPONENTE);
+		myBinNum = myBinNum.replaceAll("\\.", "");
+		int index =myBinNum.indexOf("1");
+		String mantisa = myBinNum.substring(index +1);
+
+
+		//		myBinNum = result.substring(0,myBinNum.indexOf("1")) + "." + result.substring(myBinNum.indexOf("1")+1);
+		result = sign + expBin + mantisa;
+		return HexaConverter.decimalToBase(HexaConverter.baseToDecimal(result, 2),16);
+
+
+	}
 
 
 }
--- ums-core/src/test/java/com/fiuba/taller/ums/TestFPUnit.java
+++ ums-core/src/test/java/com/fiuba/taller/ums/TestFPUnit.java
@@ -10,9 +10,27 @@ public class TestFPUnit {
 
 	//Pasar de un punto flotante en hexa a decimal
 	@Test
-	public void testPuntoFlotanteADecimal() {
+	public void testPuntoFlotanteADecimalPositivo() {
 		float result = FPUnit.puntoFlotanteADecimal("51");
-		assertEquals(4.25, result, 0.0000001);
+		assertEquals(2.125, result, 0.0000001);
+	}
+	
+	@Test
+	public void testPuntoFlotanteADecimalNegativo() {
+		float result = FPUnit.puntoFlotanteADecimal("A9");
+		assertEquals(-0.390625, result, 0.0000001);
+	}
+	
+	@Test
+	public void testDecimalAPuntoFlotantePositivo() {
+		String result = FPUnit.decimalAPuntoFlotante((float) 2.125);
+		assertEquals("51", result);
+	}
+	
+	@Test
+	public void testDecimalAPuntoFlotanteNegativo() {
+		String result = FPUnit.decimalAPuntoFlotante((float) -0.390625);
+		assertEquals("A9", result);
 	}
 
 }
